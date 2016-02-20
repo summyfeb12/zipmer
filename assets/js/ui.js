ea5505ccf6b0e80f3301b70eafe85d36e3c676fc
Gautam Krishnan <gkthegr8@gmail.com>
2016-02-20 04:51:35 -0600

    First UX Template

---
+++ assets/js/ui.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$(".left-desktop").css("width", $(window).width() - 260);
+	$("#header").css("height", $(window).height());
+	$("#map").css("height", $(window).height());
+	$("#map").css("width", "75%");
+	$(".machinery").css("width", "25%");
+	$(".machinery").css("height", $(window).height());
+});
